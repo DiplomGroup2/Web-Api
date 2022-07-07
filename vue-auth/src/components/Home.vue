73fbd0f67a96abe11e984cbb884f9d7535bc776e
ElinaNi <nikolaevaelina1@gmail.com>
2022-07-07 14:11:45 +0300

    Добавила код на Vue
    Co-Authored-By: AnnetKo-art <92692297+AnnetKo-art@users.noreply.github.com>

---
+++ vue-auth/src/components/Home.vue
@@ -0,0 +1,19 @@
+
+<template>
+<div>
+    <h3 v-if="user">Hi,{{user.first_name}} {{user.last_name}} </h3>
+    <h3 v-if="!user">You are not logged in!</h3>
+    
+</div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+export default {
+    name:'HOME',
+    
+    computed: {
+        ...mapGetters(['user'])
+    }
+}
+</script>
